a32f2f50298065df986c28ab35e6d3cbf02fa25a
MANUEL ADRIAN CHAVEZ SANCHEZ <manuel.adrian.chavez@ibm.com>
2022-04-27 17:18:09 -0500

    format the files

--- src/components/explanation/explanation-styles.js
+++ src/components/explanation/explanation-styles.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const ExplanationContext = styled.p`
 	color: #870080;
 	font-weight: 400;
-	font-family: sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI',
-		'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-		'Helvetica Neue';
+	font-family: sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+		'Fira Sans', 'Droid Sans', 'Helvetica Neue';
 	font-size: 1.5rem;
 `;
